aa58d5b9edc16a0841ff9ce68fa3fa8db8a820f2
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-05-06 10:59:39 +0200

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className='text-my-white bg-my-black px-4 py-2'>
+      <div className='container mx-auto text-center flex items-center justify-between lg:px-10'>
+        <p className='text-3xl text-left'>
+          Cine<span className='font-bold text-my-red'>Magic</span>
+        </p>
+        <a
+          href='https://developers.themoviedb.org/3/'
+          target='_blank'
+          className='transition-colors hover:text-my-red'
+        >
+          Used API
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
