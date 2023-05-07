ef93a7512dedd7924dd4c1582a17fb1d527ae0ec
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-05-07 16:31:53 +0200

    update tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,12 @@ export default {
       colors: {
         'my-white': 'rgb(250, 250, 250)',
         'my-red': '#F9252C',
-        'my-black': '#160102',
+        'my-black': '#010101',
+        darken: 'rgba(0, 0, 0, 0.95)',
       },
       backgroundImage: {
         'hero-pattern':
-          'linear-gradient(to bottom, rgba(0,0,0,0) 20%, rgba(0,0,0,1)), linear-gradient(to top, rgba(0,0,0,0) 10%, rgba(0,0,0,0.7)), url(src/assets/header-image.jpg)',
+          'linear-gradient(to bottom, rgba(0,0,0,0) 20%, rgba(0,0,0,1)), linear-gradient(to top, rgba(0,0,0,0) 20%, rgba(0,0,0,0.85)), url(src/assets/header-image.webp)',
       },
     },
   },
